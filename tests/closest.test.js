a24b5c3115f15b9b667937e0444ce1fc16b4ea02
JustinAllard <justinallard2001@gmail.com>
2021-04-30 18:17:29 -0400

    Add prime numbers array

--- tests/closest.test.js
+++ tests/closest.test.js
@@ -1,1 +1,3 @@
 const { test, expect } = require('@jest/globals');
+
+test('getRandomPrimes() should return an array containing 1 random prime between 1 and 1,000,000 when no params are passed', () => {});
